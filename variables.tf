bb32e7c69bb331f34470e8e9215f82d380d5b245
S HYS <34961313+s-hys31@users.noreply.github.com>
2025-01-02 16:05:18 +0900

    feat: add SAML provider ARN variable to Terraform configuration

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "domain" {
   description = "The domain name to find a certificate"
   type        = string
 }
+
+variable "saml_provider_arn" {
+  description = "The ARN of the SAML provider"
+  type        = string
+}
