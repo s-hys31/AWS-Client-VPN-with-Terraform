2f1e1b034f976ab493b72112b1be15749112175c
S HYS <34961313+s-hys31@users.noreply.github.com>
2025-01-02 17:07:48 +0900

    feat: enhance SAML provider ARN variables for improved authentication clarity

--- variables.tf
+++ variables.tf
@@ -4,6 +4,11 @@ variable "domain" {
 }
 
 variable "saml_provider_arn" {
-  description = "The ARN of the SAML provider"
+  description = "The ARN of the SAML provider for authentication"
+  type        = string
+}
+
+variable "self_service_saml_provider_arn" {
+  description = "The ARN of the SAML provider for self-service authentication"
   type        = string
 }
