f71b1c0c451fe09a6437cbfaebd2cbc2285733e8
S HYS <34961313+s-hys31@users.noreply.github.com>
2025-01-02 16:07:26 +0900

    feat: add Client VPN endpoint and associated resources to Terraform configuration

---
+++ vpn.tf
@@ -0,0 +1,61 @@
+resource "aws_ec2_client_vpn_endpoint" "main" {
+  description            = "Main Client VPN Endpoint"
+  server_certificate_arn = data.aws_acm_certificate.issued.arn
+  client_cidr_block      = "10.0.0.0/16"
+
+  vpc_id = aws_vpc.main.id
+
+  split_tunnel          = false
+  session_timeout_hours = 24
+  vpn_port              = 443
+
+  authentication_options {
+    type                       = "federated-authentication"
+    saml_provider_arn          = data.aws_iam_saml_provider.sp.arn
+  }
+
+  connection_log_options {
+    enabled = false
+  }
+
+  security_group_ids = [aws_security_group.vpn.id]
+}
+
+data "aws_iam_saml_provider" "sp" {
+  arn = var.saml_provider_arn
+}
+
+resource "aws_security_group" "vpn" {
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = [aws_subnet.private.cidr_block]
+  }
+}
+
+resource "aws_ec2_client_vpn_network_association" "vpn_to_private" {
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.main.id
+  subnet_id              = aws_subnet.private.id
+}
+
+resource "aws_ec2_client_vpn_authorization_rule" "allow_all_users" {
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.main.id
+  target_network_cidr    = "0.0.0.0/0"
+  authorize_all_groups   = true
+}
+
+resource "aws_ec2_client_vpn_route" "vpn_to_private" {
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.main.id
+  destination_cidr_block = "0.0.0.0/0"
+  target_vpc_subnet_id   = aws_subnet.private.id
+}
