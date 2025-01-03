9d047b290bcff83d8053250d3597accb7c36b1a1
S HYS <34961313+s-hys31@users.noreply.github.com>
2025-01-03 17:00:34 +0900

    feat: add VPC, subnets, internet gateway, NAT gateway, and route tables for VPN configuration

---
+++ modules/vpc/main.tf
@@ -0,0 +1,80 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.name_prefix} VPN VPC"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "${var.name_prefix} VPN Private Subnet"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "${var.name_prefix} VPN Public Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.name_prefix} VPN IGW"
+  }
+}
+
+resource "aws_route_table" "vpc_to_internet" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "${var.name_prefix} VPN Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_to_internet" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.vpc_to_internet.id
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = var.aws_eip_nat_id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "${var.name_prefix} VPN NAT Gateway"
+  }
+}
+
+resource "aws_route_table" "private_to_nat" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "${var.name_prefix} VPN Private Route Table"
+  }
+}
+
+resource "aws_route_table_association" "private_to_nat" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private_to_nat.id
+}
