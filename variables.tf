28fcd3196464805e5b5e807fccbef63224cd9871
S HYS <34961313+s-hys31@users.noreply.github.com>
2025-01-03 20:56:05 +0900

    feat: add variable for Elastic IP associated with NAT Gateway in variables.tf

--- variables.tf
+++ variables.tf
@@ -12,3 +12,8 @@ variable "self_service_saml_provider_arn" {
   description = "The ARN of the SAML provider for self-service authentication"
   type        = string
 }
+
+variable "aws_eip_nat_ip" {
+  description = "The Elastic IP to associate with the NAT Gateway"
+  type        = string
+}
