1e92d4573b169c54484912583e9794bdff8688b3
S HYS <34961313+s-hys31@users.noreply.github.com>
2025-01-06 03:53:53 +0900

    refactor: replace VPN security group output with private subnet CIDR blocks output

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -3,7 +3,7 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
-output "security_group_vpn_id" {
-  description = "The ID of the VPN security group"
-  value       = aws_security_group.vpn.id
+output "private_subnet_cidr_blocks" {
+  description = "The CIDR blocks of the private subnets"
+  value       = aws_subnet.private.cidr_block
 }
