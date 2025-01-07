6613116b66089e89b583c0577256ef1cb8176b89
S HYS <34961313+s-hys31@users.noreply.github.com>
2025-01-08 00:29:24 +0900

    feat: add output for private subnet ID in VPC module

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -7,3 +7,8 @@ output "private_subnet_cidr_blocks" {
   description = "The CIDR blocks of the private subnets"
   value       = aws_subnet.private.cidr_block
 }
+
+output "subnet_id" {
+  description = "The ID of the subnet"
+  value       = aws_subnet.private.id
+}
